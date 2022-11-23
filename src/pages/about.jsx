572208ec0f2ae046e5f12d6536dc8b9bdac187d6
john <liming851017@gmail.com>
2022-11-23 09:29:03 +0800

    image 수정

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import Head from 'next/head'
 import Link from 'next/link'
 import clsx from 'clsx'
-
+import aboutPic from '@/images/portrait.jpg'
 import { Container } from '@/components/Container'
 import {
   TwitterIcon,
@@ -51,8 +51,8 @@ export default function About() {
         <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
           <div className="lg:pl-20">
             <div className="max-w-xs px-2.5 lg:max-w-none">
-              <img
-                src={'/images/portrait.jpg'}
+              <Image
+                src={aboutPic}
                 alt=""
                 sizes="(min-width: 1024px) 32rem, 20rem"
                 className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
