81dae239a73c2d6189194c7a61ceb8fb38997cff
john <liming851017@gmail.com>
2022-11-29 08:47:20 +0800

    Link ==> a

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -28,10 +28,10 @@ Card.Link = function CardLink({ children, ...props }) {
   return (
     <>
       <div className="absolute -inset-y-6 -inset-x-4 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl" />
-      <Link {...props}>
+      <a {...props}>
         <span className="absolute -inset-y-6 -inset-x-4 z-20 sm:-inset-x-6 sm:rounded-2xl" />
         <span className="relative z-10">{children}</span>
-      </Link>
+      </a>
     </>
   )
 }
@@ -39,7 +39,7 @@ Card.Link = function CardLink({ children, ...props }) {
 Card.Title = function CardTitle({ as: Component = 'h2', href, children }) {
   return (
     <Component className="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
-      {href ? <Card.Link href={href} as={href}>{children}</Card.Link> : children}
+      {href ? <Card.Link href={href}>{children}</Card.Link> : children}
     </Component>
   )
 }
