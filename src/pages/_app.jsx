9194350cbf785fa128a2ed40e5d886f964a9e98b
john <liming851017@gmail.com>
2022-11-29 10:12:10 +0800

    gtag.js added

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,6 +2,7 @@ import { useEffect, useRef } from 'react'
 
 import { Footer } from '@/components/Footer'
 import { Header } from '@/components/Header'
+import Script from "next/script";
 
 import '@/styles/tailwind.css'
 import 'focus-visible'
@@ -21,6 +22,16 @@ export default function App({ Component, pageProps, router }) {
 
   return (
     <>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-RPS52K2YBR"></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-RPS52K2YBR');
+        `}
+      </Script>
       <div className="fixed inset-0 flex justify-center sm:px-8">
         <div className="flex w-full max-w-7xl lg:px-8">
           <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
